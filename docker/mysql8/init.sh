45b2d9852abf8c01722bad642a894d6b02d6a51a
lzj <18841685054@163.com>
2020-04-02 09:12:02 -0700

    ch

--- docker/mysql8/init.sh
+++ docker/mysql8/init.sh
@@ -1,9 +1,36 @@
 #!/bin/bash
 
+if [ "$1" = 'init' ]; 
+then
 docker run \
-        --name mysql_3307 \
-        -p 3307:3306 \
-        --restart always \
-        -v $PWD/root:/root \
-        -e MYSQL_ROOT_PASSWORD=123456 \
-        -d mysql \
+    --name mysql_3307 \
+    -p 3307:3306 \
+    --restart always \
+    -v $PWD/root:/root \
+    -e MYSQL_ROOT_PASSWORD=123456 \
+    -d mysql
+echo 
+elif [ "$1" = 'in' ]; 
+then
+docker exec -it mysql_3307 /bin/bash
+elif [ "$1" = 'conn' ]; 
+then
+mycli -h 172.17.0.2 -p 3306 -u root  
+else
+echo "no match option!!!"
+fi
+
+# 备份数据库
+#mysqldump -h 172.17.0.2 -P 3306 -u 'root' -p --databases server > /root/dump/server.sql
+#mysqldump -h 172.17.0.2 -P 3306 -u 'root' -p --databases main > /root/dump/main.sql
+#mysqldump -h 172.17.0.2 -P 3306 -u 'root' -p --databases statdb > /root/dump/statdb.sql
+
+# 从备份还原数据库(一定要进入到容器里边操作)
+# mysql>> create databases main;
+# mysql>> use main;
+# mysql>> source /root/dir/??.sql;
+
+# 配置远程登录 
+# 1:CREATE USER 'root'@'%' IDENTIFIED BY 'root';
+# 2:GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+# 3:flush privileges;
--- docker/mysql8/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker exec -it mysql_3307 /bin/bash
