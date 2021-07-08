a5d636da4c9ed0e533c31bf3cbd2a0719822f695
liuzijian <liuzijian@3975.com>
2021-07-08 16:34:38 +0800

    init tmux

--- tmux/init.sh
+++ tmux/init.sh
@@ -1,12 +1,54 @@
 #!/bin/bash
 
-tagdir="./Env/"
+echo "init tmux.conf"
+cat >"~/.tmux.conf" <<EOF
+#C-b is not acceptable -- Vim uses it
+set-option -g prefix C-w
 
-workdir=$(
-    cd $(dirname $0)
-    pwd
-)
+set-window-option -g mode-keys vi
+set-window-option -g utf8 on
 
-cp $workdir"/tmux.conf" ~/.tmux.conf
-cp $workdir"/session_name.sh" ~/tmux.sh
-chmod +x ~/tmux.sh
+# set window split
+bind-key v split-window -h
+bind-key b split-window
+
+# Start numbering at 1
+set -g base-index 1
+setw -g mode-keys vi
+
+set -g renumber-windows on
+set -g pane-base-index 1
+
+bind-key k select-pane -U
+bind-key j select-pane -D
+bind-key h select-pane -L
+bind-key l select-pane -R
+EOF
+
+echo "init tmux.sh"
+
+cat >"~/tmux.sh" <<EOF
+#!/bin/bash
+name=$1
+tmux_open() {
+	if [ -z  ]; then
+		tmux new -s  -d
+		tmux rename-window -t  srv
+
+        # if want to split vertical window
+    	#tmux split-window -h
+
+		tmux new-window -n oth
+		tmux select-window -t oth
+
+		# run command example
+        #tmux send-keys -t "open:1.2" 'htop' C-m
+		tmux select-window -t srv
+
+		tmux attach -t 
+	else
+		tmux attach -t  
+	fi
+}
+tmux_open
+EOF
--- tmux/session_name.sh
+++ tmux/session_name.sh
@@ -1,27 +1,21 @@
 #!/bin/bash
-
-# example
-# tmux send-keys -t "open:1.4" './run-adminserver.sh' C-m
-
 name=$1
-
 tmux_open() {
 	if [ -z $(tmux ls | grep {name}:) ]; then
 		tmux new -s ${name} -d
 		tmux rename-window -t ${name} srv
-    	tmux split-window -h
+		tmux split-window -h
 
 		tmux new-window -n oth
 		tmux select-window -t oth
 		tmux split-window -h
 
-        #tmux send-keys -t "open:1.2" 'htop' C-m
+		#tmux send-keys -t "open:1.2" 'htop' C-m
 		tmux select-window -t srv
 
 		tmux attach -t ${name}
 	else
-		tmux attach -t ${name} 
+		tmux attach -t ${name}
 	fi
 }
-
 tmux_open
