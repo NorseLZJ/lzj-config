45b6168321bf037e5235e6c57a2457fb05dac9f9
liuzijian <52354783+NorseLZJ@users.noreply.github.com>
2021-07-12 10:34:50 +0800

    Update init.sh
    ch

--- tmux/init.sh
+++ tmux/init.sh
@@ -29,26 +29,30 @@ echo "init tmux.sh"
 
 cat >~/tmux.sh <<EOF
 #!/bin/bash
-name=$1
-tmux_open() {
-	if [ -z  ]; then
-		tmux new -s  -d
-		tmux rename-window -t  srv
 
-        # if want to split vertical window
-    	#tmux split-window -h
+# example
+# tmux send-keys -t "open:1.4" './run-adminserver.sh' C-m
+
+name=$1
+
+tmux_open() {
+	if [ -z $(tmux ls | grep $name) ]; then
+		tmux new -s $name -d
+		tmux rename-window -t $name srv
+    		#tmux split-window -h
 
 		tmux new-window -n oth
 		tmux select-window -t oth
+		tmux split-window -h
 
-		# run command example
-        #tmux send-keys -t "open:1.2" 'htop' C-m
+	        #tmux send-keys -t "open:1.2" 'htop' C-m
 		tmux select-window -t srv
 
-		tmux attach -t 
+		tmux attach -t $name
 	else
-		tmux attach -t  
+		tmux attach -t $name
 	fi
 }
+
 tmux_open
 EOF
