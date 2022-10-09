57045aa806e1f28efcf8fd295b0114dec456c7a2
NorseLZJ <52354783+NorseLZJ@users.noreply.github.com>
2022-10-09 15:46:02 +0800

    Update session_name.sh

--- tmux/session_name.sh
+++ tmux/session_name.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
+
 name=$1
+
 tmux_open() {
-	if [ -z $(tmux ls | grep {name}:) ]; then
-		tmux new -s ${name} -d
-		tmux rename-window -t ${name} srv
-		tmux split-window -h
+        if [ -z $(tmux ls | grep name) ]; then
+                tmux new -s $name -d
+                tmux rename-window -t $name srv
+                #tmux split-window -h
 
-		tmux new-window -n oth
-		tmux select-window -t oth
-		tmux split-window -h
+                tmux new-window -n oth
+                tmux select-window -t oth
+                #tmux split-window -h
 
-		#tmux send-keys -t "open:1.2" 'htop' C-m
-		tmux select-window -t srv
+                #tmux send-keys -t "open:1.2" 'htop' C-m
+                tmux select-window -t srv
 
-		tmux attach -t ${name}
-	else
-		tmux attach -t ${name}
-	fi
+                tmux attach -t $name
+        else
+                tmux attach -t $name
+        fi
 }
+
 tmux_open
